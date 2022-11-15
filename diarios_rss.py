4f68d53b971b9d161865ed0bfefde5066e086e9d
morispolanco <morispolanco@gmail.com>
2022-11-14 19:14:12 -0600

    Update diarios_rss.py

--- diarios_rss.py
+++ diarios_rss.py
@@ -2,6 +2,6 @@ diarios={0:{'diario':'Prensa Libre',
     'rss': 'https://www.prensalibre.com/feed/'} ,
     1: {'diario':'Telam',
     'seccion':'Economia',
-    'rss': 'https://www.telam.com.ar/rss2/economia.xml' },
+    'rss': 'https://www.telam.com.ar/rss2/economia.xml' }
     } 
 }
